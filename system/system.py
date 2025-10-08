3dc23ce76338cfb676b7887a08fe128a3d83cf76
pyadminru <zh.vitaliy92@yandex.ru>
2025-10-08 22:09:03 +0300

    Формирование проекта

---
+++ system/system.py
@@ -0,0 +1,8 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Загружаем переменные окружения из .env
+
+# Bot token can be obtained via https://t.me/BotFather
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
