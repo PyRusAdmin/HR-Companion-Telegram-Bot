8f0d1912bcf73f1b11bbcd66c77b78e1e8fb6d60
pyadminru <zh.vitaliy92@yandex.ru>
2025-10-08 23:01:08 +0300

    Формирование меню бота для HR

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
+from loguru import logger
 from keyboards.keyboards import employee_menu_keyboard
 from system.system import BOT_TOKEN, dp
 
@@ -17,6 +17,16 @@ async def command_start_handler(message: Message) -> None:
     """
     Отвечает на команду /start
     """
+
+    # Фиксируем пользователей, которые отправили команду /start
+    id_user = message.from_user.id  # id пользователя
+    username = message.from_user.username  # username пользователя
+    first_name = message.from_user.first_name  # имя пользователя
+    last_name = message.from_user.last_name  # фамилия пользователя
+
+    logger.info(f"Пользователь c id {id_user} username {username} first_name {first_name} last_name {last_name} отправил команду /start")
+
+    # Отправляем приветственное сообщение пользователю
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!", reply_markup=employee_menu_keyboard())
 
 
